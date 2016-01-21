dce861cb8559673249f323d31e9f49844fafc660
Jumabek <jumabek4044@gmail.com>
2016-01-22 03:44:57 +0900

    modify python/caffe/classifier.py by adding 'extract' function

--- python/caffe/classifier.py
+++ python/caffe/classifier.py
@@ -95,3 +95,56 @@ class Classifier(caffe.Net):
             predictions = predictions.mean(1)
 
         return predictions
+
+    def extract(self, inputs,blob, oversample=True):
+        """
+        Predict classification probabilities of inputs.
+
+        Parameters
+        ----------
+        inputs : iterable of (H x W x K) input ndarrays.
+        oversample : boolean
+            average predictions across center, corners, and mirrors
+            when True (default). Center-only prediction when False.
+
+        Returns
+        -------
+        predictions: (N x C) ndarray of class probabilities for N images and C
+            classes.
+        """
+    # Scale to standardize input dimensions.
+        input_ = np.zeros((len(inputs),
+                           self.image_dims[0],
+                           self.image_dims[1],
+                           inputs[0].shape[2]),
+                          dtype=np.float32)
+        for ix, in_ in enumerate(inputs):
+            input_[ix] = caffe.io.resize_image(in_, self.image_dims)
+
+        if oversample:
+            # Generate center, corner, and mirrored crops.
+            input_ = caffe.io.oversample(input_, self.crop_dims)
+        else:
+            # Take center crop.
+            center = np.array(self.image_dims) / 2.0
+            crop = np.tile(center, (1, 2))[0] + np.concatenate([
+                -self.crop_dims / 2.0,
+                self.crop_dims / 2.0
+            ])
+            input_ = input_[:, crop[0]:crop[2], crop[1]:crop[3], :]
+
+        # Classify
+        caffe_in = np.zeros(np.array(input_.shape)[[0, 3, 1, 2]],
+                            dtype=np.float32)
+        for ix, in_ in enumerate(input_):
+            caffe_in[ix] = self.transformer.preprocess(self.inputs[0], in_)
+        out = self.forward_all(**{self.inputs[0]: caffe_in,'blobs':[blob]})
+        extractions = out[blob]
+    
+    # For oversampling, average predictions across crops.
+        if oversample:
+            extractions = extractions.reshape((len(extractions) / 10, 10, -1))
+            extractions = extractions.mean(1)
+    
+        return extractions
+
