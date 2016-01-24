51fff6c32a996b9fd22e8419310fbb264cf27a56
Jumabek <jumabek4044@gmail.com>
2016-01-24 13:18:37 +0900

    add some test for last batch

--- python/extract_features.py
+++ python/extract_features.py
@@ -192,6 +192,7 @@ def main(argv):
                         id=num_batch*batch_size+e
                         datum.label=int(labels[id])
                         str_id = '{:08}'.format(id)
+                        print(str_id)
                         txn.put(str_id,datum.SerializeToString())
                         
 #         if num_batch==0:
